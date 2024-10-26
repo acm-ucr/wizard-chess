f209db08678118a0157f74fdc2cd649fb0dc81d5
Marcus Hsieh <80995617+MarcusHsieh@users.noreply.github.com>
2024-10-25 17:25:09 -0700

    Game screen chessboard \\ Screen framework

--- embeddings/mainwindow.cpp
+++ embeddings/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 MainWindow::~MainWindow()
@@ -15,6 +16,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(2);
+    ui->stackedWidget->setCurrentIndex(3);
 }
+
 
