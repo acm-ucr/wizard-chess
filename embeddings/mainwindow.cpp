2a056440cdd9718d8358b43e390a29dfc7ebc013
VigneshT24 <vignesh.tho2006@gmail.com>
2024-11-13 15:09:08 -0800

    Finalized and fixed bugs on QT

--- embeddings/mainwindow.cpp
+++ embeddings/mainwindow.cpp
@@ -24,7 +24,7 @@ void MainWindow::on_pushButton_18_clicked()
 }
 
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 void MainWindow::on_pushButton_19_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
@@ -34,19 +34,17 @@ void MainWindow::on_pushButton_19_clicked()
 void MainWindow::on_pushButton_2_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
-=======
+}
+
 void MainWindow::on_pushButton_start_clicked()
 {
     ui->stackedWidget->setCurrentIndex(1);
 }
 
-
-
 void MainWindow::on_pushButton_tutorial_clicked()
 {
     ui->stackedWidget->setCurrentIndex(2);
 }
-
 
 void MainWindow::on_pushButton_settings_clicked()
 {
@@ -57,6 +55,6 @@ void MainWindow::on_pushButton_settings_clicked()
 void MainWindow::on_pushButton_about_clicked()
 {
     ui->stackedWidget->setCurrentIndex(5);
->>>>>>> ec80273e3b344ef1a23e998207c077d80fe31758
+// >>>>>>> ec80273e3b344ef1a23e998207c077d80fe31758
 }
 
--- embeddings/mainwindow.h
+++ embeddings/mainwindow.h
@@ -23,11 +23,11 @@ private slots:
 
     void on_pushButton_18_clicked();
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
     void on_pushButton_19_clicked();
 
     void on_pushButton_2_clicked();
-=======
+// =======
     void on_pushButton_start_clicked();
 
     void on_pushButton_tutorial_clicked();
@@ -35,7 +35,7 @@ private slots:
     void on_pushButton_settings_clicked();
 
     void on_pushButton_about_clicked();
->>>>>>> ec80273e3b344ef1a23e998207c077d80fe31758
+// >>>>>>> ec80273e3b344ef1a23e998207c077d80fe31758
 
 private:
     Ui::MainWindow *ui;
