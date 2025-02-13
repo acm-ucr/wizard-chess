a2ffd40c114517ed5869eeedb6a4ddba54f1fe83
Austin Le <ale193@ucr.edu>
2025-02-12 17:52:04 -0800

    Adding txt file

--- software/voicerecognition.py
+++ software/voicerecognition.py
@@ -5,11 +5,10 @@ recognizer = sr.Recognizer()
 def listen_for_commands():
    try:
       with sr.Microphone() as source:
-         ##recognizer.adjust_for_ambient_noise(source, duration=0.2)
-         audio = recognizer.listen(source)
+         recognizer.adjust_for_ambient_noise(source, duration=0.5)
+         audio = recognizer.listen(source, phrase_time_limit=None)
          text = recognizer.recognize_google(audio)
          print("text recognized\n")
-
          return text
 
    except sr.RequestError as e:
@@ -29,11 +28,16 @@ def parsedData(userInput):
     chess_piece = re.findall(r"(pawn|queen|rook|bishop|knight|king|night|Pawn|Queen|Rook|Bishop|Knight|King|Night)", userInput)
     locations = re.findall(r"[A-H][1-8]", userInput)
     print("PARSED DATA: ", chess_piece, locations)
-    if(len(chess_piece + locations) == 3): 
-        data = {"piece": chess_piece[0], "start": locations[0], "end": locations[1]}
-        return data
+    file_path = 'speech.txt'
+    if(len(locations) == 2): 
+        res = ' '.join(locations)
+        print(res)
+        file = open(file_path, 'w')
+        file.write(res) 
     else:
         error = {"piece" : "ERROR"}
+        file = open(file_path, 'w')
+        file.write("ERROR")
         return error
 
 # Main program
@@ -47,7 +51,7 @@ while (1):
         print(userInput)
         print("\n")
         resultingData = parsedData(userInput) # stores a hashmap with key data
-        if (resultingData["piece"] != "ERROR"):
-            print("Moving [" + resultingData["piece"] + "] from position [" + resultingData["start"] + "] to [" + resultingData["end"] + "]")
-        else:
-            print("Invalid input. You might have referred to one or more coordinate/piece.")
+        # if (resultingData["piece"] != "ERROR"):
+        #     print("Moving [" + resultingData["piece"] + "] from position [" + resultingData["start"] + "] to [" + resultingData["end"] + "]")
+        # else:
+        #     print("Invalid input. You might have referred to one or more coordinate/piece.")
