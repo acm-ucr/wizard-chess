e7c00eef90f8fc55f36058c4f730da379fdc1dd4
iracema clark <emilyclark5293@gmail.com>
2025-03-05 13:48:59 -0800

    Added limit switch code -- need to debug

--- embeddings/motors/motorsync.ino
+++ embeddings/motors/motorsync.ino
@@ -1,3 +1,5 @@
+#include <ezButton.h>
+
 //Used to send the number of steps
 #define motorX1 2
 #define motorX2 3
@@ -10,6 +12,15 @@
 
 //Turned to HIGH by default. Recommend attaching a 2 pin jumper cap between EN and GND on the CNC Shield.
 #define enableMotors 8 
+
+// define limit switches
+// #define limitSwitch1 9  // motorX1
+// #define limitSwitch2 10 // motorX2
+// #define limitSwitch3 11 // motorY
+
+ezButton limitSwitch1(9);  // create ezButton object that attach to pin 7;
+ezButton limitSwitch2(10);
+ezButton limitSwitch3(11);
 
 //motorStep = Step Pin on Driver
 //steps = Amount of Steps to Travel (200 Steps = 1 Rotation given 1.8 Degree Step Angle and Full Steps Enabled)
@@ -26,7 +37,16 @@ void step(int motorStep, int steps, int delayTime){
 
 void vertical_mvmt(int num_steps, bool direction) {
   digitalWrite(motorY, direction);
-  step(motorY, num_steps, 100);
+      
+  for(int i = 0; i < num_steps; i++) {
+    limitSwitch3.loop();
+    if (limitSwitch3.isPressed()) {
+      Serial.println("switch pressed");
+      break;
+    }  
+    Serial.println("switch not pressed");
+    step(motorY, 1, 1);
+  }
 }
 
 void horizontal_mvmt(int num_steps, bool direction) {
@@ -34,8 +54,25 @@ void horizontal_mvmt(int num_steps, bool direction) {
   digitalWrite(motorX1, direction);
   digitalWrite(motorX2, !direction);
   for(int i = 0; i < num_steps; i++) {
-    step(motorX1, 1, 10);
-    step(motorX2, 1, 10);
+    limitSwitch1.loop();
+    limitSwitch2.loop();
+    if (limitSwitch1.isPressed()) {
+      Serial.println("switch pressed");
+      break;
+    }
+    else {
+      Serial.println("switch not pressed");
+      step(motorX1, 1, 1);
+    }
+    if (limitSwitch2.isPressed()) {
+      Serial.println("switch pressed");
+      break;
+    }
+    else {
+      Serial.println("switch not pressed");
+      step(motorX2, 1, 1);
+      
+    }
   }
 }
 
@@ -107,23 +144,35 @@ void setup(){
 	pinMode(motorX1Dir, OUTPUT);
 	pinMode(motorX2Dir, OUTPUT);
 	pinMode(motorYDir, OUTPUT);
-	
-	//Enables the motors
+
+  //Enables the motors
 	digitalWrite(8, LOW);
 
+  limitSwitch1.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitch2.setDebounceTime(50);
+  limitSwitch3.setDebounceTime(50);
+
+  // pinMode(limitSwitch1, INPUT);
+  // pinMode(limitSwitch2, INPUT);
+  // pinMode(limitSwitch3, INPUT);
+	
   Serial.begin(9600);
   while(!Serial) {
     ;
   }
 }
 
-int curr_x_pos = 50;
-int curr_y_pos = 50;
+int curr_x_pos = 0;
+int curr_y_pos = 0;
 
 void loop(){
   // currently assuming 800 x 800 tick board
   // each square is 100 x 100
   // input format: "x1 y1 x2 y2\n"
+
+  // limitSwitch1.loop(); // MUST call the loop() function first
+  // limitSwitch2.loop();
+  // limitSwitch3.loop();
 
   String input_coords;
 
