47f254b2325f21fedfa1fb529f24dfda1138860c
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-05-14 16:59:57 -0700

    Update piCom.py

--- piCom.py
+++ piCom.py
@@ -3,7 +3,6 @@ import time
 
 # start serial write
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-
 time.sleep(2)
 
 # helper function to read each content in a single line
@@ -27,35 +26,37 @@ is_capture = False
 castle_check = ""
 promotion_check = ""
 
-
-file = open("qt.txt")
-
 # reading the content in "qt.txt" file 
 with open("qt.txt", "r") as file:
-    id_num = read.until(file, " ");
-    time_stamp = read.until(file, " ");
-    origCoord = read.until(file, " ");
-    newCoord = read.until(file, " ");
-    is_capture = read.until(file, " ");
-    castle_check = read.until(file, " ");
-    promotion_check = read.until(file, " ");
-    
-sendLine = id_num + '#'
-sendLine += time_stamp + '#'
-sendLine += origCoord + '#'
-sendLine += newCoord + '#'
-sendLine += is_capture + '#'
-sendLine += castle_check + '#'
-sendLine += promotion_check
+    id_num         = read_until(file, " ")
+    time_stamp     = read_until(file, " ")
+    origCoord      = read_until(file, " ")
+    newCoord       = read_until(file, " ")
+    is_capture     = read_until(file, " ")
+    castle_check   = read_until(file, " ")
+    promotion_check= read_until(file, " ")
 
-ser.write((str(sendLine) + '\n').encode('utf-8'))
+# build the line to send
+sendLine = (
+    id_num + "#" +
+    time_stamp + "#" +
+    origCoord + "#" +
+    newCoord + "#" +
+    is_capture + "#" +
+    castle_check + "#" +
+    promotion_check
+)
 
-# personal confirmation (to be deleted)
-# print(f"Arduino response: {response}")
+# send to Arduino
+ser.write((sendLine + "\n").encode("utf-8"))
 
-# writing to the file
+# —— M I N O R   F I X ——  
+# now actually read back whatever the Arduino sends (e.g. UID, time, status, etc.)
+response = ser.readline().decode("utf-8").strip()
+print(f"Arduino response: {response}")
+
+# append Arduino response to qt.txt
 with open("qt.txt", "a") as file:
     file.write("\n" + response + "\n")
 
 ser.close()
-
